3aaef659882dd18b6882e5812ff7924018612b8c
shiguanWXX <1358110409@qq.com>
2018-09-30 18:16:36 +0800

    按任务、月份统计图表（钉钉）

--- src/page/task/task.js
+++ src/page/task/task.js
@@ -1,99 +1,155 @@
 
-const chartDataNew = [{
-  name: '完成',
-  task: '备耕',
-  num: 500
-}, {
-    name: '进行中',
-    task: '备耕',
-    num: 300
-  }, {
-    name: '逾期',
-    task: '备耕',
-    num: 30
-  }, {
-    name: '完成',
-    task: '移栽',
-    num: 550
-  }, {
-    name: '进行中',
-    task: '移栽',
-    num: 200
-  }, {
-    name: '逾期',
-    task: '移栽',
-    num: 10
-  },
-  {
-    name: '完成',
-    task: '田管',
-    num: 800
-  }, {
-    name: '进行中',
-    task: '田管',
-    num: 700
-  }, {
-    name: '逾期',
-    task: '田管',
-    num: 10
-  },
-  {
-    name: '完成',
-    task: '质保病种害',
-    num: 800
-  }, {
-    name: '进行中',
-    task: '质保病种害',
-    num: 700
-  }, {
-    name: '逾期',
-    task: '质保病种害',
-    num: 10
-  },
-  {
-    name: '完成',
-    task: '烘烤',
-    num: 800
-  }, {
-    name: '进行中',
-    task: '烘烤',
-    num: 700
-  }, {
-    name: '逾期',
-    task: '烘烤',
-    num: 10
-  },
-  {
-    name: '完成',
-    task: '分级',
-    num: 800
-  }, {
-    name: '进行中',
-    task: '分级',
-    num: 700
-  }, {
-    name: '逾期',
-    task: '分级',
-    num: 10
-  }]
+import Dropdown from '../../components/dropdown';
+// const chartDataNew = [{
+//   name: '完成',
+//   task: '备耕',
+//   num: 500
+// }, {
+//     name: '进行中',
+//     task: '备耕',
+//     num: 300
+//   }, {
+//     name: '逾期',
+//     task: '备耕',
+//     num: 30
+//   }, {
+//     name: '完成',
+//     task: '移栽',
+//     num: 550
+//   }, {
+//     name: '进行中',
+//     task: '移栽',
+//     num: 200
+//   }, {
+//     name: '逾期',
+//     task: '移栽',
+//     num: 10
+//   },
+//   {
+//     name: '完成',
+//     task: '田管',
+//     num: 800
+//   }, {
+//     name: '进行中',
+//     task: '田管',
+//     num: 700
+//   }, {
+//     name: '逾期',
+//     task: '田管',
+//     num: 10
+//   },
+//   {
+//     name: '完成',
+//     task: '质保病种害',
+//     num: 800
+//   }, {
+//     name: '进行中',
+//     task: '质保病种害',
+//     num: 700
+//   }, {
+//     name: '逾期',
+//     task: '质保病种害',
+//     num: 10
+//   },
+//   {
+//     name: '完成',
+//     task: '烘烤',
+//     num: 800
+//   }, {
+//     name: '进行中',
+//     task: '烘烤',
+//     num: 700
+//   }, {
+//     name: '逾期',
+//     task: '烘烤',
+//     num: 10
+//   },
+//   {
+//     name: '完成',
+//     task: '分级',
+//     num: 800
+//   }, {
+//     name: '进行中',
+//     task: '分级',
+//     num: 700
+//   }, {
+//     name: '逾期',
+//     task: '分级',
+//     num: 10
+//   }]
 
 let app = getApp()
 
 Page({
+  ...Dropdown,
   data: {
     width: 200,
     height: 200,
     chart: null,
+    startDate: '',
+    endDate: '',
+    items: [],
+    dropdownSelectData: {
+      active: false,
+      selectedNav: 0,
+      listData: [
+        {
+          nav: '开始日期',
+          selectedItem: '',
+          type: 'button',
+          data: []
+        },
+        {
+          nav: '结束日期',
+          selectedItem: '',
+          type: 'button',
+          data: []
+        },
+      ],
+    },
   },
   onLoad() {
+    this.getNowFormatDate();
   },
   onReady() {
 
   },
+  //获取默认时间（当月的第一天和最后一天）
+  getNowFormatDate() {
+    var date = new Date();
+    var seperator1 = "-";
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+      month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+      strDate = "0" + strDate;
+    }
+    var currentdate = year + seperator1 + month + seperator1 + strDate;
+    var startDate = year + seperator1 + month + seperator1 + '01';
+    this.setData({
+      startDate: startDate,
+      endDate: currentdate
+    });
+    let alistData = this.data.dropdownSelectData.listData;
+    alistData[0].nav = startDate;
+    alistData[1].nav = currentdate;
+    this.setData({
+      dropdownSelectData: {
+        ...this.data.dropdownSelectData,
+        listData: alistData
+      }
+    });
+  },
   onDraw(ddChart, F2) {
     //dd-charts组件内部会回调此方法，返回图表实例ddChart
     //提示：可以把异步获取数据及渲染图表逻辑放onDraw回调里面
-    ddChart.clear()
+
+    this.getDistrictChartData(ddChart,F2);
+    /*ddChart.clear()
+   
     ddChart.source(chartDataNew, {
       population: {
         tickCount: 5
@@ -145,6 +201,137 @@ Page({
     });
 
     ddChart.interval().position('task*num').color('name', ['#1890FF', '#13C2C2', '#FE5D4D']).adjust('stack');
-    ddChart.render()
-  }
+    ddChart.render()*/
+  },
+  getDistrictChartData(ddChart,F2) {
+    dd.showLoading();
+    dd.httpRequest({
+      url: app.globalData.host + 'api/services/app/Chart/GetChartByGroupAsync',
+      method: 'Get',
+      data: {
+        // userId: app.globalData.userInfo.id,
+        startTime: this.data.startDate,
+        endTime: this.data.endDate
+      },
+      dataType: 'json',
+      success: (res) => {
+        this.setData({items: res.data.result.items })
+        const chartDataNew = this.data.items;
+        if(!this.data.chart){
+          ddChart.clear()
+
+          ddChart.source(chartDataNew, {
+            population: {
+              tickCount: 5
+            }
+          })
+          ddChart.coord({
+            transposed: true
+          })
+          ddChart.axis('task', {
+            line: F2.Global._defaultAxis.line,
+            grid: null
+          })
+          ddChart.axis('num', {
+            line: null,
+            grid: F2.Global._defaultAxis.grid,
+            label: function label(text, index, total) {
+              var textCfg = {};
+              if (index === 0) {
+                textCfg.textAlign = 'left';
+              } else if (index === total - 1) {
+                textCfg.textAlign = 'right';
+              }
+              return textCfg;
+            }
+          })
+          ddChart.tooltip({
+            custom: true, // 自定义 tooltip 内容框
+            onChange: function onChange(obj) {
+              var legend = ddChart.get('legendController').legends.top[0];
+              var tooltipItems = obj.items;
+              var legendItems = legend.items;
+              var map = {};
+              legendItems.map(function(item) {
+                map[item.name] = _.clone(item);
+              });
+              tooltipItems.map(function(item) {
+                var name = item.name;
+                var value = item.value;
+                if (map[name]) {
+                  map[name].value = value;
+                }
+              });
+              legend.setItems(_.values(map));
+            },
+            onHide: function onHide() {
+              var legend = ddChart.get('legendController').legends.top[0];
+              legend.setItems(ddChart.getLegendItems().country);
+            }
+          });
+
+          ddChart.interval().position('district*num').color('name', ['#1890FF', '#13C2C2', '#FE5D4D']).adjust('stack');
+          ddChart.render()
+          this.data.chart = ddChart;
+        }else{
+          ddChart.changeData(chartDataNew);
+        }
+      },
+      fail: function(res) {
+        dd.alert({ content: '获取数据异常' });
+        console.log('error:', res);
+      },
+      complete: function(res) {
+        dd.hideLoading();
+      }
+    });
+  },
+
+  onButtonNavItemTap(e, index) {
+    const { selectedNav, active } = this.data.dropdownSelectData;
+    let nextactive = !active;
+    if (selectedNav !== index) {
+      nextactive = true;
+    }
+
+    this.setData({
+      dropdownSelectData: {
+        ...this.data.dropdownSelectData,
+        active: nextactive,
+        selectedNav: index,
+      }
+    });
+    const alistData = this.data.dropdownSelectData.listData;
+    //弹出日历
+    dd.datePicker({
+      format: 'yyyy-MM-dd',
+      currentDate: index == 0 ? this.data.startDate : this.data.endDate,
+      startDate: index == 0 ? '' : this.data.startDate,
+      endDate: index == 0 ? this.data.endDate : '',
+      success: (res) => {
+        alistData[index].nav = res.date;
+        if (index == 0) {
+          this.data.startDate = res.date;
+        } else {
+          this.data.endDate = res.date;
+        }
+        this.setData({
+          dropdownSelectData: {
+            ...this.data.dropdownSelectData,
+            active: false,
+            listData: alistData
+          }
+        });
+        this.getDistrictChartData(this.data.chart);
+      },
+      fail: (res) => {
+        this.setData({
+          dropdownSelectData: {
+            ...this.data.dropdownSelectData,
+            active: false
+          }
+        })
+      },
+    });
+  },
 })
