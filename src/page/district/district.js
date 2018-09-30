5c895af691408c7390491f43f916d7d92201754c
Donald <tangdezou@qq.com>
2018-09-30 10:48:08 +0800

    刷新图表处理

--- src/page/district/district.js
+++ src/page/district/district.js
@@ -104,7 +104,9 @@ Page({
   onDraw(ddChart) {
     //dd-charts组件内部会回调此方法，返回图表实例ddChart
     //提示：可以把异步获取数据及渲染图表逻辑放onDraw回调里面
-    ddChart.clear()
+    this.getDistrictChartData(ddChart);
+  },
+  getDistrictChartData(ddChart){
     dd.showLoading();
     dd.httpRequest({
       url: app.globalData.host + 'api/services/app/Chart/GetDistrictChartDataAsync',
@@ -118,49 +120,55 @@ Page({
       success: (res) => {
         this.setData({ districts: res.data.result.districts, items: res.data.result.items })
         const chartDataNew = this.data.items;
-        ddChart.source(chartDataNew)
-        ddChart.tooltip({
+        if(!this.data.chart){
+          ddChart.clear()
+          ddChart.source(chartDataNew)
+          ddChart.tooltip({
 
-          custom: true, // 自定义 tooltip 内容框
-          onChange: function onChange(obj) {
-            var legend = chart.get('legendController').legends.top[0];
-            var tooltipItems = obj.items;
-            var legendItems = legend.items;
-            var map = {};
-            legendItems.map(function(item) {
-              map[item.name] = _.clone(item);
-            });
-            tooltipItems.map(function(item) {
-              var name = item.name;
-              var value = item.value;
-              if (map[name]) {
-                map[name].value = value;
+            custom: true, // 自定义 tooltip 内容框
+            onChange: function onChange(obj) {
+              var legend = chart.get('legendController').legends.top[0];
+              var tooltipItems = obj.items;
+              var legendItems = legend.items;
+              var map = {};
+              legendItems.map(function(item) {
+                map[item.name] = _.clone(item);
+              });
+              tooltipItems.map(function(item) {
+                var name = item.name;
+                var value = item.value;
+                if (map[name]) {
+                  map[name].value = value;
+                }
+              });
+              legend.setItems(_.values(map));
+            },
+            onHide: function onHide() {
+              var legend = chart.get('legendController').legends.top[0];
+              legend.setItems(chart.getLegendItems().country);
+            }
+          })
+          ddChart.axis('date', {
+            label(text, index, total) {
+              const textCfg = {};
+              if (index === 0) {
+                textCfg.textAlign = 'left';
               }
-            });
-            legend.setItems(_.values(map));
-          },
-          onHide: function onHide() {
-            var legend = chart.get('legendController').legends.top[0];
-            legend.setItems(chart.getLegendItems().country);
-          }
-        })
-        ddChart.axis('date', {
-          label(text, index, total) {
-            const textCfg = {};
-            if (index === 0) {
-              textCfg.textAlign = 'left';
+              if (index === total - 1) {
+                textCfg.textAlign = 'right';
+              }
+              return textCfg;
             }
-            if (index === total - 1) {
-              textCfg.textAlign = 'right';
-            }
-            return textCfg;
-          }
-        })
-        ddChart.interval().position('district*num').color('name', ['#1890FF', '#13C2C2', '#FE5D4D']).adjust({
-          type: 'dodge',
-          marginRatio: 0.05 // 设置分组间柱子的间距
-        })
-        ddChart.render()
+          })
+          ddChart.interval().position('district*num').color('name', ['#1890FF', '#13C2C2', '#FE5D4D']).adjust({
+            type: 'dodge',
+            marginRatio: 0.05 // 设置分组间柱子的间距
+          })
+          ddChart.render()
+          this.data.chart = ddChart;
+        } else {
+          ddChart.changeData(chartDataNew);
+        }
       },
       fail: function(res) {
         dd.alert({ content: '获取数据异常' });
@@ -205,6 +213,15 @@ Page({
             listData: alistData
           }
         });
+        this.getDistrictChartData(this.data.chart);
+      },
+      fail: (res) => {
+        this.setData({
+          dropdownSelectData: {
+            ...this.data.dropdownSelectData,
+            active: false
+          }
+        })
       },
     });
   },
