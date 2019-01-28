bebe1451437290cf8f06f874ca43a9f19f47b7dc
shiguanWXX <1358110409@qq.com>
2019-01-28 10:00:08 +0800

    修改连接地址

--- src/app.js
+++ src/app.js
@@ -14,8 +14,8 @@ App({
   globalData: {
     userInfo: { id: '', name: '', position: '', avatar: '' },
     // host: 'http://gyapi.intcov.com/',
-    host: 'http://hechuangdd.vaiwan.com/',
-    // host: 'http://127.0.0.1:21021/',
+    // host: 'http://hechuangdd.vaiwan.com/',
+    host: 'http://127.0.0.1:21021/',
     corpId:'',
     appId: 8
   }
